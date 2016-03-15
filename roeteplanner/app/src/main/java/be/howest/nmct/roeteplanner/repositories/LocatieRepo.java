91f62cc2d8ecf11d25c57c3139c65351ec4f5d1b
Hein Pauwelyn <hein_pauwelyn@hotmail.com>
2016-03-15 10:02:45 +0100

    fill read methode

--- roeteplanner/app/src/main/java/be/howest/nmct/roeteplanner/repositories/LocatieRepo.java
+++ roeteplanner/app/src/main/java/be/howest/nmct/roeteplanner/repositories/LocatieRepo.java
@@ -1,11 +1,16 @@
 package be.howest.nmct.roeteplanner.repositories;
 
+import android.content.Context;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import be.howest.nmct.roeteplanner.classes.Locatie;
 
 public class LocatieRepo {
     private ArrayList<Locatie> _locaties = new ArrayList<Locatie>();
+    private final String _bestandsnaam = "Roeteplaner_locaties.csv";
 
     public ArrayList<Locatie> getLocaties() {
         return _locaties;
@@ -21,6 +26,17 @@ public class LocatieRepo {
 
     public void readLocaties(){
 
+        String tekst = "";
+
+        try {
+
+            //FileOutputStream fos = openFileOutput(_bestandsnaam, Context.MODE_PRIVATE);
+            //fos.write(tekst.getBytes());
+            //fos.close();
+        }
+        catch (IOException ex) {
+            System.out.print(ex.getMessage());
+        }
     }
 
     public void writeLocaties(){
